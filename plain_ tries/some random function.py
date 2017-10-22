cedeca890de7175f78a69147f5ff682b398ebd1b
igordecc <Igordecc@yandex.ru>
2017-10-22 22:09:12 +0400

    plain tries to work with python

---
+++ plain_ tries/some random function.py
@@ -0,0 +1,19 @@
+print(dict(((1,2),),),) # delete 2 birgule and explain why
+
+#trying to execute "coolfunction" gived with input
+def dict_operations(onedict, anotherdict, coolfunction):
+    try: cooolfunction(onedict, another)
+    except: print('eto gg')
+    # print('nu chto, pognali?')
+    return 42
+
+one = dict(( (1,"!"), (2,"@"), (3,"#") ))
+another = dict(( (4,"$"), (5,"%"), (6,"^") ))
+
+#we will test lambda functuon, that suppose to summ two dictionaries
+f = dict_operations(one, another, lambda x,y: x+y)
+print(f)
+
+#print(one+another) #<- so, it's not gona work
+input("press key, 'cause why not")
+
