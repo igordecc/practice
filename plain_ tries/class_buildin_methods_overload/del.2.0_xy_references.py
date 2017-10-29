7ba1929bb662cd6dedf428b8017e0005dcc5faf3
igordecc <Igordecc@yandex.ru>
2017-10-29 19:55:15 +0400

    fix some comments in del.2.0

--- plain_ tries/class_buildin_methods_overload/del.2.0_xy_references.py
+++ plain_ tries/class_buildin_methods_overload/del.2.0_xy_references.py
@@ -3,7 +3,7 @@ class my_class():
         print('destroy start    and at the end of the programm we delete it')
         print('self = ' + str(self))
         #self = None
-        print( 'self = '+str(self)+"    but delete it after method __del__(), 'cause we rewrited __del__()")
+        print( 'self = '+str(self)+"    but delete it after method __del__(), 'cause we rewrited __del__() without 'self = None' part")
 
 if __name__ == '__main__':
     x = my_class()
@@ -12,5 +12,5 @@ if __name__ == '__main__':
     try:
         print(x)
     except:
-        print('NameError' + "   fist - after del we fix, that there is no 'x'")
+        print('NameError' + "   fist - after del() we fix, that there is no 'x'")
     print(str(y) + " but we don't delete it!    <-str(y)")
