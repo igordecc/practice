9a50a75efec8d3d7465e41318e8e64d63cbd51d6
The Notebook <Igordecc@yandex.ru>
2019-07-16 14:43:51 +0400

    add App.js

--- _html_css_js/react_calculator/src/App.js
+++ _html_css_js/react_calculator/src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ResultComponent from '.components/ResultComponent';
+import KeyPadComponent from '.components/KeyPadComponent';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    constructor(){
+      super();
+
+      this.state = {
+        result: ""
+      }
+    }
+
+    render() {
+        return (
+            <div>
+              <div className="calculator-body">
+                <h1>Simple Calculator</h1>
+                <ResultComponent result={this.state.result}/>
+                <KeyPadComponent onClick={this.onClick}/>
+              </div>
+            </div>
+        )
+    }
 }
 
 export default App;
