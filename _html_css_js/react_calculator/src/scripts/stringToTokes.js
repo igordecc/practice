10299b5e61e2efa0c60a3cfc495073c54478fb6c
The Notebook <Igordecc@yandex.ru>
2019-07-19 23:36:49 +0400

    calculator update
    add evaluate function. Fix RPN.

--- _html_css_js/react_calculator/src/scripts/stringToTokes.js
+++ _html_css_js/react_calculator/src/scripts/stringToTokes.js
@@ -1,65 +1,72 @@
 // import React, { Component } from 'react';
 
+function compileRPN(tokenRPNstring){
+    var operatorStack = [];
+    var operandStack = [];
+    var pendingOperandTrigger = 0;
 
-function stringToTokens(_string) {
-    var stack = [];
-    var num_stack = "";
-    var dot_stack = [];
-
-    for (var i = 0; i < _string.length; i++) {
-
-        switch (_string[i]){
-            case /[0-9]/:
-                num_stack.concat(_string[i]); // push to stack
-                break;
-            case /\./:
-                if (!dot_stack[0]) {
-                    dot_stack.push(".")
-                } else {
-                    return "syntax error: False dot"
-                }
-                break;
-            case /\s/: break;
-            case /[+\-*/\s]/ || i !== _string.length:
-                let token = parseFloat(num_stack);
-                dot_stack = [];
-                stack.push(token);
-                break;
-            default:
-                // console.log(typeof _string[i]);
+    function evaluate(operator, operand1, operand2) {
+        var operand1 = parseFloat(operand1);
+        var operand2 = parseFloat(operand2);
+        switch (operator) {
+            case "+":
+                return operand1+operand2;
+            case "-":
+                return operand1-operand2;
+            case "*":
+                return operand1*operand2;
+            case "/":
+                return operand1/operand2;
+            case "^":
+                return Math.pow(operand1, operand2);
+            default: throw "Unidentified operator "+operator;
         }
     }
-    return stack
-}
 
-function compileRPN(_string){
-    const operators = {
-        "+": (x,y) => x+y,
-        "-": (x,y) => x-y,
-        "*": (x,y) => x*y,
-        "/": (x,y) => x/y,
-    };
-    let evaluate = (_string) => {
-        let stack = [];
-
-        _string.split(" ").forEach((token) => {
-            if (token in operators) {
-                let [x, y] = [stack.pop(), stack.pop()];;
-                let result = operators[token](x, y)
-                stack.push(result);
-            } else {
-                stack.push(parseFloat(token));
+    for (let i=tokenRPNstring.length-1; i >= 0; i--) { //from right to left evaluate RPN version
+        var token = tokenRPNstring[i][0];
+        // console.log(token);
+        let id = tokenRPNstring[i][1];
+        if (id === "op") {
+            var operator = token;
+            // console.log(operator);
+            operatorStack.push(operator);
+            pendingOperandTrigger = 0;
+        } else if (id === "num") {
+            var operand = token;
+            if (pendingOperandTrigger) {
+                while ((operandStack.length)
+                    && operatorStack.length){
+                    var operand1 = operandStack.pop();
+                    var operator = operatorStack.pop();
+                    console.log(operand);
+                    console.log(operand1);
+                    console.log(operator);
+                    var operand = evaluate(operator, operand, operand1);
+                }
             }
-        });
-        return stack;
-    };
-    return evaluate(_string);
+        operandStack.push(operand);
+        pendingOperandTrigger = 1;
+        }
+    }
+    return "done: "+operandStack
 }
 
 function myParser(_string){
     /*
     * return list of tokens, which are (!) string type (!)
     * ALWAYS USE TRY-CATCH on myParser
+    *
+    * tokenStack[0] - token by itself
+    * tokenStack[1] index legend:
+        * num - number,
+        * op - operator,
+        * func - function,
+        * ( - left parent
+        * ) - right parent
+        *tokenStack[2] - operator precedence: ^ - 4, /* - 3, +- - 2.
+        *tokenStack[3] - operator associations: l - left, r - right
+    *
     * */
     var stack = [];
 
@@ -67,12 +74,12 @@ function myParser(_string){
 
     var e_trigger = 0;
     var dot_trigger = 0;
-    var operation_type_trigger = "u"; // u - unary, b - binary
+    var operation_type_trigger = "u"; // u - unary, b - binary, e - exponent
 
     function pushNumTokenAndResetTriggers() {
         if (token_num_buffer[0]) { // because we need to avoid empty stack tokens, which are if (token_num_buffer[0] === null)
             let complete_token = token_num_buffer.join("");
-            stack.push(complete_token);
+            stack.push([complete_token, "num"]);
         }
         e_trigger = 0;
         dot_trigger = 0;
@@ -102,36 +109,110 @@ function myParser(_string){
             } else if (operation_type_trigger === "b") {
                 // push to stack and reset token buffer;
                 pushNumTokenAndResetTriggers();
-                stack.push(_string[i])
+                stack.push([_string[i], "op", 2]) // stack[2] is the operator precedence
             }
         } else if (_string[i] === "(" || _string[i] === ")")   {
             pushNumTokenAndResetTriggers();
-            stack.push(_string[i]);
             if (_string[i] === "(") {
                 operation_type_trigger = "u";
+                stack.push([_string[i], "("]);
+            } else {
+                stack.push([_string[i], ")"]);
             }
-        } else if ((_string[i] === "*")||
-                (_string[i] === "/")||
-                (_string[i] === "^"))
+        } else if ((_string[i] === "*")
+                ||(_string[i] === "/")
+                )
         { //simple operation identifier
             pushNumTokenAndResetTriggers();
-            stack.push(_string[i]);
+            stack.push([_string[i], "op", 3]); // stack[2] is the operator precedence
+        } else if (_string[i] === "^") {
+            pushNumTokenAndResetTriggers();
+            stack.push([_string[i], "op", 4]); // stack[2] is the operator precedence
         }
     }
     pushNumTokenAndResetTriggers();
-    // TODO catch braces, not close braces, ending operators
+    // TODO catch not close braces, ending operators INSIDE reverse poland notation
     // TODO lets return nut just stack, but stack divided  to numbers, operators and functions (unary operators).
     // Note lets not add multi-argument functions
     return stack;
 }
 
 
-var _str = "+87 ++++ (-895e+40 + 89895 * 859859.0999e-10) ";
+function tokensToPolishNotation(tokenStack) {
+
+    var queue = [];
+    var operatorStack = [];
+    for (let i = 0; i<tokenStack.length; i++) {
+        /*
+        * tokenStack[0] - token by itself
+        * tokenStack[1] - index legend:
+            * num - number,
+            * op - operator,
+            * func - function,
+            * ( - left parent
+            * ) - right parent
+           *tokenStack[2] - operator precedence: ^ - 4, /* - 3, +- - 2.
+           *tokenStack[3] - operator associations: l - left, r - right
+        */
+        let token = tokenStack[i];
+        let id = tokenStack[i][1];
+        if ( id === "num") {
+            queue.push(token)
+        } else if ( id === "func" ) {
+            operatorStack.push(token)
+        } else if ( id === "op") { // the complecatest part
+            if (operatorStack[operatorStack.length-1]) { // because if operatorStack is empty js refuse to compare it (see lower).
+                while (
+                    ((operatorStack[operatorStack.length-1][1] === "func")
+                        || (operatorStack[operatorStack.length-1][1] === "op" && operatorStack[operatorStack.length-1][2] > token[2] ) //or (there is an operator at the top of the operator stack with greater precedence)
+                        || (operatorStack[operatorStack.length-1][1] === "op" && operatorStack[operatorStack.length-1][2] === token[2] && operatorStack[operatorStack.length-1][3] === "l")
+                    )
+                    && (operatorStack[operatorStack.length-1][1] !== "(")
+                    ) {
+                    queue.push( operatorStack.pop() );
+                }
+            }
+            operatorStack.push(token);
+        } else if ( id === "(" ) {
+            operatorStack.push(token)
+        } else if ( id === ")" ) {
+            if (operatorStack[operatorStack.length-1]) {
+                while (
+                    operatorStack[operatorStack.length-1][1] !== "("
+                    ) {
+                    if (!operatorStack) {throw "mismatched parentheses";}
+                    queue.push( operatorStack.pop() );
+                }
+            }
+            if (operatorStack[operatorStack.length-1]) {
+                if (operatorStack[operatorStack.length-1][1] === "(") {
+                    operatorStack.pop();
+                }
+            }
+            if (operatorStack[0]) { // "if operatorStack is not null"
+                while (operatorStack.length) { queue.push(operatorStack.pop()) }
+            } else if (!tokenStack.length){
+                while (operatorStack.length) {
+                    if (operatorStack[operatorStack.length-1][1] === "(" || operatorStack[operatorStack.length-1][1]===")") {
+                        throw "left open parentheses";
+                    }
+                    queue.push(operatorStack.pop())
+                }
+            }
+        }
+    }
+    return queue;
+}
+
+var _str = "+87 ++++ (-895e+40 + 89895^2 * 859859.0999e-10) ";
 try {
-    console.log(myParser(_str));
+    var infix = myParser(_str);
+    console.log(infix);
+    var postfix = tokensToPolishNotation(infix);
+    console.log(postfix);
+    console.log(compileRPN(postfix));
 }
 catch(e) {
     console.log(e)
 }
-
 // export default toReversePolishNotation;++++++++++++++++
