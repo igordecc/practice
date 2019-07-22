2c10e67151ae3a3f8484e6a9459a7218fa2493b8
igordecc <Igordecc@yandex.ru>
2019-07-22 15:54:05 +0400

    Add profile of chai tests
    Need to make up in future

--- _html_css_js/react_calculator/src/scripts/stringToTokens.test.js
+++ _html_css_js/react_calculator/src/scripts/stringToTokens.test.js
@@ -1,6 +1,18 @@
-import React, {Component} from 'react';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
 import calculateString from "./stringToTokens";
 
 
+// it('Add', calculateString("5+9"){
+//     const div = document.createElement('div');
+//     ReactDOM.render(<App />, div);
+//     ReactDOM.unmountComponentAtNode(div);
+// });
 
-console.log(calculateString("5+9"));
+var assert = chai.assert();
+
+var testFunction = calculateString
+assert.equal(testFunction("5+9"), "14")
+
+
