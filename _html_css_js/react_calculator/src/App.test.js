089778f01bfece96c713c29104e57fa8fa94dad5
The Notebook <Igordecc@yandex.ru>
2019-07-24 04:59:27 +0400

    Add tests and rearange code blocks

--- _html_css_js/react_calculator/src/App.test.js
+++ _html_css_js/react_calculator/src/App.test.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import calculateString from './scripts/stringToTokens.js';
+
 import ResultComponent from './components/ResultComponent.js'
 import KeyPadComponent from './components/KeyPadComponent.js'
 
 import chai from 'chai';
-var assert = chai.assert;
-assert.typeOf("foo", 'string');
+const assert = chai.assert;
+
 assert.equal(1, 1);
-assert.lengthOf([1,2,3], 3);
+// assert.typeOf("foo", 'string');
+// assert.lengthOf([1,2,3], 3);
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
@@ -27,27 +28,4 @@ it('KeyPad component is OK', () => {
   const div = document.createElement('div');
   ReactDOM.render(<KeyPadComponent/>, div);
   ReactDOM.unmountComponentAtNode(div);
-});
-
-//calculate 2.0
-var calculate = (_string) => {return calculateString(_string)};
-
-it('calculate: addition +', () => {
-  assert.equal(calculate("3+2"), "5");
-});
-
-it('calculate: subtraction -', () => {
-  assert.equal(calculate("3-2"), "1");
-});
-
-it('calculate: multiplication *', () => {
-  assert.equal(calculate("3*2"), "6");
-});
-
-it('calculate: division /', () => {
-  assert.equal(calculate("3/2"), "1.5");
-});
-
-it('calculate: multi addition ', () => {
-  assert.equal(calculate("+87 ++++ (-895e+40 + 89895^2 * 859859.0999e-10) "), "-8.95e+42");
 });
--- _html_css_js/react_calculator/src/scripts/stringToTokens.test.js
+++ _html_css_js/react_calculator/src/scripts/stringToTokens.test.js
@@ -1,18 +1,49 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import chai from 'chai';
 
 import calculateString from "./stringToTokens";
 
 
-// it('Add', calculateString("5+9"){
-//     const div = document.createElement('div');
-//     ReactDOM.render(<App />, div);
-//     ReactDOM.unmountComponentAtNode(div);
-// });
+const assert = chai.assert;
+const calculate = (_string)=>{return calculateString(_string)};
 
-var assert = chai.assert();
+// const assert = chai.assert;
+// assert.typeOf("foo", 'string');
+// assert.equal(1, 1);
+// assert.lengthOf([1,2,3], 3);
 
-var testFunction = calculateString;
-assert.equal(testFunction("5+9"), "14")
 
+it('calculate: addition +', () => {
+    assert.equal(calculate("3+2"), "5");
+});
+
+it('calculate: subtraction -', () => {
+    assert.equal(calculate("3-2"), "1");
+});
+
+it('calculate: multiplication *', () => {
+    assert.equal(calculate("3*2"), "6");
+});
+
+it('calculate: division /', () => {
+    assert.equal(calculate("3/2"), "1.5");
+});
+
+it('calculate: addition with parens ()', () => {
+    assert.equal(calculate("(3+2)"), "5");
+});
+
+it('calculate: multi addition only', () => {
+    assert.equal(calculate("87 ++++ 3 "), "90");
+});
+
+
+it('calculate: multi addition ', () => {
+    assert.equal(calculate("+87 ++++ (-895e+40 + 89895^2 * 859859.0999e-10) "), "-8.95e+42");
+});
+
+
+it('calculate: multi brackets ', () => {
+    assert.equal(calculate("+87 ++++ (((-895e+40-10)*5) + (89895^2 * 859859.0999e-10)) "), "-8.95e+42");
+});
 
