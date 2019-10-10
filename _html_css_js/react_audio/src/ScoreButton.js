9be8d63b8fdd0c15b0a4ec8f9484621dd98c6a49
igordecc <Igordecc@yandex.ru>
2019-10-11 01:00:22 +0400

    Update react audio app

---
+++ _html_css_js/react_audio/src/ScoreButton.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+function ScoreButton () {
+    return (
+        <div>
+            <h2>label</h2>
+            <input type="button" value="your score is: "></input>
+        </div>
+    )
+};
+
+export default ScoreButton;
--- _html_css_js/react_audio/src/index.js
+++ _html_css_js/react_audio/src/index.js
@@ -1,14 +1,22 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom'
-import { render } from 'react-dom'
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { render } from 'react-dom';
 
 /* Import Components */
-import About from './About'
-import App from './App'
+import About from './About';
+import App from './App';
+import Labyrinth from './Labyrinth';
+import ScoreButton from './ScoreButton';
 
 render((
   <BrowserRouter>
-    <Route exact path='/' component={App}/>
-    <Route path='/About' component={About}/>
+    <Switch>
+      <Route exact path='/' component={App}/>
+      <Route path='/about' component={About}/>
+      <Route exact path='/labyrinth'>
+        <Labyrinth />
+      </Route>
+      <Route exact path='/labyrinth/ScoreButton' component={ScoreButton}/>
+    </Switch>
   </BrowserRouter>
 ), document.getElementById('root'));
