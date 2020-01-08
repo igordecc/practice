d32a106dcf18c363e08905fefcb6d03ab4b61183
igordecc <Igordecc@yandex.ru>
2020-01-08 22:06:38 +0400

    Clean up code and comments of compound_graph

--- python_networkx/Compound_graph.py
+++ python_networkx/Compound_graph.py
@@ -2,41 +2,40 @@ import networkx
 import matplotlib.pyplot as plt
 
 
-def add_graphs(graph_list):
+def add_graphs(graphs):
     """
     create graph from multiple graphs
-    :param graph_list:
+    :param graphs:
     :return:
     """
 
-    summed_graph = networkx.Graph()
+    accumulator_graph = networkx.Graph()
 
-    low_border = 0
+    # This is the number of a node, which will be connected to a previous graph. This is also used to shift the number
+    # of each node in each consequent graph, so they dont collide with the numbers which are already present in
+    # accumulator.
+    next_graph_node = 0
 
-    for i in range(len(graph_list)):
+    for i, graph in enumerate(graphs):
 
-        # extract edges from graph
+        edges = graph.edges()
         # update nodes numbers
-        edges = graph_list[i].edges()
-        new_edges = [(edge[0] + low_border, edge[1] + low_border) for edge in edges]
+        new_edges = [(n1 + next_graph_node, n2 + next_graph_node) for n1, n2 in edges]
 
-        # add bound
+        # connect to previous graph
         if i > 0:
-            bound_edge = [(low_border - 1, low_border)]
-            new_edges += bound_edge
+            new_edges.append( (next_graph_node - 1, next_graph_node) )
 
-        # update low_border
-        for edge in new_edges:
-            for node in edge:
-                if node >= low_border:
-                    low_border = node + 1
+        # find max_node_number
+        # new graph start's from max_node_number
+        next_graph_node = max((n for e in new_edges for n in e)) + 1   # flat map
 
-        summed_graph.add_edges_from(new_edges)
+        accumulator_graph.add_edges_from(new_edges)
 
-    # final edge to cycle it all
-    summed_graph.add_edges_from([(0, low_border)])
+    # connect the last node to the first
+    accumulator_graph.add_edges_from([(0, next_graph_node - 1)])
 
-    return summed_graph
+    return accumulator_graph
 
 
 
