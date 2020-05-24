5f6e9ddd3c0841571e793d913b1cb5d348096a28
igordecc <Igordecc@yandex.ru>
2020-05-24 11:09:48 +0400

    Add user reference and personal paths

--- python_reg/index.py
+++ python_reg/index.py
@@ -10,7 +10,7 @@ def hello():
     # user render
     hello_page = ""
     for user in USERS.keys():
-        hello_page += (f"<li><a href='/{...}'>{user}</a></li>")
+        hello_page += (f"<li><a href='/{user}'>{user}</a></li>")
     hello_page = f"<ul>{hello_page}</ul>"
     # greetings render
     hello_page = f"<h2>WELLCOME TO HOMEPAGE</h2><br>{hello_page}"
@@ -28,18 +28,25 @@ def login():
     '''
 
 
+@route('/<username>')
+def route_user(username):
+    return f"THERE WILL BE {username} DATA"
+
+
 @post('/login') # or @route('/login', method='POST')
 def do_login():
     username = request.forms.get('username')
     password = request.forms.get('password')
 
     def check_login(username, password):
-        print(username)
-        print(password)
         return True
 
     if check_login(username, password):
         USERS.update({username: password})
+
+
+
+        route_user(username)
         return "<p>Your login information was correct.</p>"
     else:
         return "<p>Login failed.</p>"
