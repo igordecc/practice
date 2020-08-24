dc233db3d71c496bec76146058df609527bf1229
igordecc <Igordecc@yandex.ru>
2020-08-24 17:46:35 +0400

    Add typescrypt practice

---
+++ typescript/test/main.js
@@ -0,0 +1,6 @@
+"use strict";
+console.log("Hellow World");
+function hello(x) {
+    console.log(x);
+}
+hello("Nice to meet buddy");
---
+++ typescript/test/main.ts
@@ -0,0 +1,7 @@
+console.log("Hellow World");
+
+function hello(x:string){
+    console.log(x)
+}
+
+hello("Nice to meet buddy");
